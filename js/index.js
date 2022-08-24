eab9b74a088a3ed3effbc22c3a5c001a6cc8dcbc
cxrrxde <111563147+cxrrxde@users.noreply.github.com>
2022-08-24 17:12:08 +0500

    fixed some issues with buttons
    Sometimes "new card" button wasn't work fine, so now it's probably working right

--- js/index.js
+++ js/index.js
@@ -22,6 +22,9 @@ function getRandomCard(){
 
 function startGame(){
     isAlive = true
+    cards = []
+    sum = 0
+    hasBlackjack = false
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     cards = [firstCard,secondCard]
@@ -37,14 +40,37 @@ function renderGame(){
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20){
         message = "Do you want to draw a new card?"
+
+        document.querySelector("#start-btn").disabled = true
+        document.getElementById("start-btn").style.cursor = "not-allowed"
+        document.getElementById("start-btn").style.background = "#808080"
+
+        document.querySelector("#new-btn").disabled = false
+        document.getElementById("new-btn").style.cursor = "pointer"
+        document.getElementById("new-btn").style.background = "#DAA520"
     }
     else if (sum === 21){
         message = "You've got Blackjack!"
         hasBlackjack = true
+
+        document.querySelector("#start-btn").disabled = false
+        document.getElementById("start-btn").style.cursor = "pointer"
+        document.getElementById("start-btn").style.background = "#DAA520"
+
+        document.querySelector("#new-btn").disabled = true
+        document.getElementById("new-btn").style.cursor = "not-allowed"
+        document.getElementById("new-btn").style.background = "#808080"
     }
     else{
         message = "You've lost the game!"
         isAlive = false
+
+        document.querySelector("#start-btn").disabled = false
+        document.getElementById("start-btn").style.cursor = "pointer"
+        document.getElementById("start-btn").style.background = "#DAA520"
+        document.querySelector("#new-btn").disabled = true
+        document.getElementById("new-btn").style.cursor = "not-allowed"
+        document.getElementById("new-btn").style.background = "#808080"
     }
     messageEl.textContent = message
 }
